cf744c03d3057ef36fd343b4e1f190bfda814fcc
Anibal F Lopez <lopezanibalf@outlook.com>
2022-11-15 22:41:29 -0300

    entrega clase 12

---
+++ src/Mensajes.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+class Mensajes {
+  constructor(name) {
+    this.name = name;
+  }
+
+  async save(item) {
+    try {
+      let items = await this.getAll();
+      items.push(item);
+      await fs.promises.writeFile(this.name, JSON.stringify(items));
+    } catch (error) {
+      console.log("Error al guardar registro");
+      console.log(error);
+    }
+  }
+
+  async getAll() {
+    try {
+      let files = await fs.promises.readdir("./");
+
+      if (!files.includes(this.name))
+        await fs.promises.writeFile(this.name, JSON.stringify([]));
+
+      return JSON.parse(await fs.promises.readFile(this.name, "utf-8"));
+    } catch (error) {
+      console.log("Error al obtener los registros");
+      console.log(error);
+    }
+  }
+}
+
+module.exports = Mensajes;
--- src/server.js
+++ src/server.js
@@ -2,19 +2,35 @@ const path = require("path");
 const express = require("express");
 const { Router } = express;
 const Contenedor = require("./Contenedor");
+const Mensajes = require("./Mensajes");
+const handlebars = require("express-handlebars");
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
 
 const PORT = 8080;
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 const routerProductos = Router();
 const productos = new Contenedor();
+const mensajes = new Mensajes("mensajes.txt");
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+//app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api/productos", routerProductos);
 
 app.get("/", (req, res) => {
-  res.send("index.html");
+  res.render("carga");
+});
+
+app.get("/productos", (req, res) => {
+  const listado = productos.getAll();
+  res.render("listado", { productos: listado, noVacio: listado.length > 0 });
 });
 
 routerProductos.get("/", (req, res) => {
@@ -30,7 +46,9 @@ routerProductos.get("/:id", (req, res) => {
 
 routerProductos.post("/", (req, res) => {
   const id = productos.save(req.body);
-  res.json(id);
+  //res.json(id);
+  io.emit("productos", productos.getAll());
+  res.send();
 });
 
 routerProductos.put("/:id", (req, res) => {
@@ -45,6 +63,23 @@ routerProductos.delete("/:id", (req, res) => {
   else res.json({ error: "Producto no encontrado" }).status(404);
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("Usuario conectado");
+  socket.emit("productos", productos.getAll());
+
+  (async function () {
+    const msjs = await mensajes.getAll();
+    socket.emit("mensajes", msjs);
+  })();
+
+  socket.on("mensaje", async (mensaje) => {
+    mensaje.fecha = new Date().toLocaleString();
+    await mensajes.save(mensaje);
+    const msjs = await mensajes.getAll();
+    io.emit("mensajes", msjs);
+  });
 });
